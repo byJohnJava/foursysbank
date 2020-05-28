8c02ff2f8a2887a9287ca4c5bbf91292a2b1183f
João Carlos Araujo Santos <jaraujo@server01.foursys.local>
2020-05-28 15:20:17 -0300

    Incluso conceito de herança e namespace

--- app/ts/views/NegociacoesView.ts
+++ app/ts/views/NegociacoesView.ts
@@ -1,47 +1,39 @@
-class NegociacoesView{
+namespace Views {
 
-    private _elemento: Element;
+    export class NegociacoesView extends Views.View<Negociacoes>{
 
-    constructor(seletor: string){
-        this._elemento = document.querySelector(seletor);
-    }
 
-    update(model: Negociacoes): void{
-       this._elemento.innerHTML = this.template(model); 
-    }
+        template(model: Negociacoes): string {
 
-    template(model: Negociacoes): string {
- 
-        return `
-            <table class="table table-hover table-bordered">
-                <thead>
-                    <tr>
-                        <th>DATA</th>
-                        <th>QUANTIDADE</th>
-                        <th>VALOR</th>
-                        <th>VOLUME</th>
-                    </tr>
-                </thead>
+            return `
+                <table class="table table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th>DATA</th>
+                            <th>QUANTIDADE</th>
+                            <th>VALOR</th>
+                            <th>VOLUME</th>
+                        </tr>
+                    </thead>
+        
+                    <tbody>
+                    ${model.retornaArray().map(negociacao =>
+                `<tr>
+                            <td>${negociacao.data.getUTCDate()}/${negociacao.data.getUTCMonth() + 1}/${negociacao.data.getFullYear()}</td>
+                            <td>${negociacao.quantidade}</td>
+                            <td>${negociacao.valor}</td>
+                            <td>${negociacao.volume}</td>
+                        </tr>                        
+                        `).join('')} 
     
-                <tbody>
-                ${model.retornaArray().map(negociacao =>
-                    `<tr>
-                        <td>${negociacao.data.getUTCDate()}/${negociacao.data.getUTCMonth() + 1}/${negociacao.data.getFullYear()}</td>
-                        <td>${negociacao.quantidade}</td>
-                        <td>${negociacao.valor}</td>
-                        <td>${negociacao.volume}</td>
-                    </tr>                        
-                    `).join('')} 
+                    </tbody>
+        
+                    <tfoot>
+                    </tfoot>
+                </table>               
+                `
+        }
 
-                </tbody>
-    
-                <tfoot>
-                </tfoot>
-            </table>               
-            `
     }
-
-
-
 
 }
